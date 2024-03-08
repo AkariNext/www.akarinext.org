ab111f802cb05bff41645e86ea5f91dcac3a5515
yupix <yupi0982@outlook.jp>
2024-03-09 06:59:52 +0900

    feat: トップページを作り直した

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -5,6 +5,8 @@ import { Button } from "~/components/ui/button";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "~/components/ui/tooltip";
 import { getSocialIcon } from "~/lib/utils";
 import { MEMBERS } from "~/lib/member.server";
+import { SERVICES } from "~/lib/services.server";
+import { ServiceCard } from "~/components/ServiceCard";
 
 export const meta: MetaFunction = () => {
   return [
@@ -14,45 +16,80 @@ export const meta: MetaFunction = () => {
 };
 
 export function loader() {
-  return json({ members: MEMBERS });
+  return json({ members: MEMBERS, services: SERVICES.slice(0, 3) });
 }
 
 
 
 export default function Index() {
-  const { members } = useLoaderData<typeof loader>();
+  const { members, services } = useLoaderData<typeof loader>();
   const displayMembers = members.slice(0, 3);  // 3人だけ表示する残りはView allで専用ページにて表示する
 
   return (
     <div>
-      <div className="grid grid-cols-1  md:grid-cols-3 w-full  gap-y-4 md:gap-x-4">
-        <div className="h-96 col-span-2 bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-slate-900 via-purple-900 to-slate-900 rounded-2xl flex flex-col p-8">
-          <div className="text-white text-4xl font-bold">Let's have fun and play funny!</div>
-          <p className="text-white mt-4">AkariNextで一緒に楽しくゲームや開発を行いましょう！</p>
-        </div>
-        <div className="col-span-1 bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-blue-700 via-blue-800 to-gray-900 rounded-2xl w-full p-8 flex flex-col justify-between">
-          <div>
-            <div className="text-white text-4xl font-bold">Join us!</div>
-            <p className="text-white mt-4">今すぐAkariNextのコミュニティーに参加しましょう！</p>
+      <div className="grid grid-cols-1  md:grid-cols-3 w-full  gap-y-4 md:gap-x-4 h-screen items-center">
+        <div className="h-96 col-span-2 rounded-lg flex flex-col p-8 order-2 sm:order-1">
+          <div className="text-4xl">Let's have fun and play funny!</div>
+          <p className="mt-4">AkariNextで一緒に楽しくゲームや開発を行いましょう！</p>
+
+          <div className="pt-8">
+            <div>
+              <Button size={"lg"} className="rounded-3xl">Read the blog</Button>
+            </div>
+            <div className="pt-4">
+              <Button variant={"outline"} size={"lg"} className="rounded-3xl">Join us</Button>
+            </div>
           </div>
-          <Button asChild>
-            <a href="https://discord.gg/E2aG9qrFmh" target="_blank" rel="noreferrer noopener">
-              Discord
-            </a>
-          </Button>
+        </div>
+        <div className="order-1 sm:order-1">
+          <img src="/party.svg" />
+        </div>
+      </div>
+      <div className="relative">
+        <div className='
+          ball
+          mt-8
+        bg-white
+          rounded-3xl
+          p-4
+
+          before:shadow
+          before:-z-10
+          before:absolute
+          before:w-96
+          before:h-96
+          before:-top-40
+          before:-left-24
+          before:rounded-full
+
+          after:shadow
+          after:-z-10
+          after:absolute
+          after:w-56
+          after:h-56
+          after:-right-0
+          after:-bottom-12
+          after:rounded-full
+        '>
+          <div className="text-slate-700 text-3xl font-bold z-40">AkariNextって何！？</div>
+          <div className="mt-8 px-8">
+            AkariNextはゲームやプログラミングといった開発に興味を持つ人たちが集まるコミュニティーです。
+
+            主にDiscordを使ってコミュニケーションを取りながら、ゲームをしたり、開発をしたりしています。
+            <br></br>
+            <br></br>
+            また、副次的な活動としてサーバーの運営や本サイト上でのブログの執筆なども行っています。
+          </div>
         </div>
       </div>
 
-      <div className="mt-8 bg-white rounded-2xl p-8 min-h-96">
-        <div className="flex justify-between items-center" id="usual-member-header">
-          <div className="text-slate-700 text-2xl font-bold">USUAL MEMBER</div>
-          <a href="#" className="text-slate-700 hover:underline hover:transition-all">View all</a>
-        </div>
+      <div className="mt-16 rounded-lg min-h-96">
+        <div className="text-slate-700 text-2xl font-bold text-center">運営メンバー</div>
 
         <div className="grid grid-cols-1 md:grid-cols-3 mt-8 gap-8">
           {displayMembers.map((member) => (
-            <div key={member.name} className="flex justify-center items-center flex-col bg-slate-100 rounded-3xl p-8">
-              <img src={member.avatar} className="h-24 rounded-2xl" alt="" />
+            <div key={member.name} className="flex justify-center items-center flex-col bg-slate-200 rounded-lg p-8">
+              <img src={member.avatar} className="h-24 rounded-lg" alt="" />
               <div className="mt-2 text-slate-700 text-lg font-bold">{member.name}</div>
               <div className="flex flex-row gap-2">
                 {member.socials.map((social, index) => (
@@ -78,12 +115,40 @@ export default function Index() {
         </div>
       </div>
 
-      <div className="mt-8 h-96 grid grid-cols-3 gap-4">
-        <div className="col-span-1 w-full h-full bg-blue-200 rounded-2xl p-8">
-          プレイしているゲームを確認しましょう！
-        </div>
-        <div className="col-span-2 w-full h-full bg-blue-400 rounded-2xl">
+      <div className="mt-8">
 
+        <div className="col-span-1 w-full h-full bg-slate-200 rounded-lg p-8">
+          <div className="text-slate-700 text-2xl font-bold text-center">
+            SERVICES
+          </div>
+
+          <p className="text-slate-600 text-center mt-4">
+            これらのサービスはAkariNextが運営するサービスのほんのすこしです。詳しくはREAD MOREからご覧ください！
+          </p>
+
+          <div className="flex flex-wrap justify-center  gap-x-16 mt-8">
+
+            {services.map((service, index) => (
+              <div key={index} className="mb-8">
+                <ServiceCard {...service} />
+              </div>
+            ))}
+          </div>
+          <div className="flex items-center flex-col">
+            <div className="mt-4">
+              <Button size={"lg"} className="rounded-3xl">READ MORE</Button>
+            </div>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-8">
+
+          <div className="w-full h-full bg-slate-200 rounded-lg p-8">
+            <div className="text-slate-700 text-2xl font-bold text-center">PLAYING GAME'S</div>
+
+          </div>
+          <div className="w-full h-full bg-slate-200 rounded-lg p-8">
+            <div className="text-slate-700 text-2xl font-bold text-center">WORKS</div>
+          </div>
         </div>
       </div>
     </div>
