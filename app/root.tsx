056c52af1439153053f2579cfb1718571370aff4
yupix <yupi0982@outlook.jp>
2024-03-09 00:47:55 +0900

    feat: layoutにnavbarとlayoutを適応

--- app/root.tsx
+++ app/root.tsx
@@ -5,6 +5,17 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import { Navbar } from "./components/navbar";
+
+import "~/tailwind.css";
+import { LinksFunction } from "@remix-run/node";
+import { Footer } from "./components/footer";
+
+export const links: LinksFunction = () => {
+  return [
+    { rel: "stylesheet", href: "https://fonts.googleapis.com/css?family=Sawarabi+Gothic" }
+  ]
+}
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -15,10 +26,18 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
+      <body className="bg-slate-100">
+        <div className="container">
+          <div className="mt-4">
+          <Navbar />
+          </div>
+          <main className="mt-8">
+            {children}
+          </main>
+          <Footer />
+          <ScrollRestoration />
+          <Scripts />
+        </div>
       </body>
     </html>
   );
