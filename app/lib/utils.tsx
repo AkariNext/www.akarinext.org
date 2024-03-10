44317337d30d6fe9292ecf78324f0ddbd70500ce
yupix <yupi0982@outlook.jp>
2024-03-10 15:18:12 +0900

    feat: getSocialIconでTablerIconsPropsを渡せるように

--- app/lib/utils.tsx
+++ app/lib/utils.tsx
@@ -1,5 +1,5 @@
 import { type ClassValue, clsx } from "clsx"
-import { IconBrandDiscord, IconBrandGithub, IconBrandSteam, IconWorld } from "@tabler/icons-react";
+import { IconBrandDiscord, IconBrandGithub, IconBrandSteam, IconWorld, TablerIconsProps } from "@tabler/icons-react";
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
@@ -8,15 +8,16 @@ export function cn(...inputs: ClassValue[]) {
 
 
 
-export function getSocialIcon(type: string) {
-    switch (type) {
-      case "discord":
-        return <IconBrandDiscord />;
-      case "github":
-        return <IconBrandGithub />;
-      case "steam":
-        return <IconBrandSteam />;
-      default:
-        return <IconWorld />;
-    }
+export function getSocialIcon(type: string, props?: TablerIconsProps) {
+  let className = cn("yx-social-icon", props?.className);
+  switch (type) {
+    case "discord":
+      return <IconBrandDiscord className={className} {...props}  />;
+    case "github":
+      return <IconBrandGithub className={className} {...props}/>;
+    case "steam":
+      return <IconBrandSteam className={className} {...props} />;
+    default:
+      return <IconWorld className={className} {...props} />;
   }
+}
