490bd6c4d8819cd39ae4a7a16fddefcc8c4f30ff
sousuke0422 <sousuke20xx@gmail.com>
2024-03-13 01:09:09 +0900

    refactor: fmt

--- tests/root.spec.ts
+++ tests/root.spec.ts
@@ -1,17 +1,17 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
 test.beforeEach(async ({ page }) => {
-    await page.goto('http://localhost:5173/');
+  await page.goto("http://localhost:5173/");
 });
 
-test.describe('Root Page', () => {
-    test('Check Title', async ({ page }) => {
-        await expect(page).toHaveTitle('AkariNext')
-    })
+test.describe("Root Page", () => {
+  test("Check Title", async ({ page }) => {
+    await expect(page).toHaveTitle("AkariNext");
+  });
 
-    test('Landing Page Text', async ({page}) => {
-        const text = 'Let\'s have fun and play funny!'
-        expect(text).toBeTruthy()
-        await page.screenshot({ path: 'screenshots/landing.png', fullPage: false });
-    })
-})
+  test("Landing Page Text", async ({ page }) => {
+    const text = "Let's have fun and play funny!";
+    expect(text).toBeTruthy();
+    await page.screenshot({ path: "screenshots/landing.png", fullPage: false });
+  });
+});
