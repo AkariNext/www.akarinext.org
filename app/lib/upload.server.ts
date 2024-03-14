cdb37af55915c0202519c13c824d0d88e73d8dfc
yupix <yupi0982@outlook.jp>
2024-03-14 03:27:16 +0000

    feat: ファイルのアップロードを処理する uploadHandlerを追加

---
+++ app/lib/upload.server.ts
@@ -0,0 +1,11 @@
+import { UploadHandler } from "@remix-run/node";
+import { convertToBuffer } from "./helper.server";
+import { objectStorage } from "./objectstorage";
+
+
+export const uploadHandler: UploadHandler = async ({ data, filename }) => {
+
+    const buffer = await convertToBuffer(data)
+    await objectStorage.upload({ filename, data: buffer, userId: "1" });
+    return "ok"
+}
