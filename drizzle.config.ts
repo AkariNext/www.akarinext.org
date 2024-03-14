2933ccb2a7038a784525c85b010b97a28be23f07
yupix <yupi0982@outlook.jp>
2024-03-14 01:28:50 +0000

    feat: drizzleのconfigを定義

---
+++ drizzle.config.ts
@@ -0,0 +1,16 @@
+import 'dotenv/config';
+import type { Config } from "drizzle-kit";
+
+export default {
+    schema: "src/schema.ts",
+    out: "./drizzle/",
+    driver: "pg",
+    dbCredentials: {
+        user: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DB,
+        host: process.env.POSTGRES_HOST ? process.env.POSTGRES_HOST : "akarinext-web-postgres.akarinext-web",  // 開発環境ではホスト名を指定する
+        port: process.env.POSTGRES_PORT ? parseInt(process.env.POSTGRES_PORT) : 5432,
+
+    }
+} satisfies Config;
