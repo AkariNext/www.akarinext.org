fe25c76fd9a3f8d6bb101cd7619530da5c14921c
yupix <yupi0982@outlook.jp>
2024-03-20 02:20:21 +0000

    feat(components): Flowコンポーネントを追加

---
+++ app/components/Flow.client.tsx
@@ -0,0 +1,18 @@
+import ReactFlow, { Background, Controls, Node, Edge } from "reactflow";
+import 'reactflow/dist/style.css';
+
+interface FlowProps {
+    edges: Edge<any>[]
+    nodes: Node<any, string | undefined>[] | undefined
+}
+
+export function Flow({ nodes, edges }: FlowProps) {
+    return (
+        <div style={{height: '300px'}}>
+            <ReactFlow nodes={nodes} edges={edges}>
+                <Background />
+                <Controls />
+            </ReactFlow>
+        </div>
+    )
+}
