b07b165973c21a23e68fcf3938964ca6a4f9b6e3
yupix <yupi0982@outlook.jp>
2024-04-27 21:08:58 +0900

    feat: サービスをホバーした際の挙動を変更

--- app/components/ServiceCard.tsx
+++ app/components/ServiceCard.tsx
@@ -6,14 +6,14 @@ type Props = {
 
 export function ServiceCard({ name, description, thumbnailUrl }: Props) {
 	return (
-		<div className="w-[250px] max-w-fit ">
+		<div className="w-[250px] max-w-fit group">
 			<div className="flex justify-center">
 				<img
 					srcSet={thumbnailUrl}
 					decoding="async"
 					loading="lazy"
 					alt={name}
-					className="h-[330px] object-cover transition-all hover:scale-95 aspect-[3/4] mb-4 rounded-md"
+					className="h-[330px] object-cover aspect-[3/4] mb-4 group-hover:ring-4 ring-offset-4 ring-offset-slate-100 transition-all rounded-md"
 				/>
 			</div>
 			<h3 className="text-xl">{name}</h3>
