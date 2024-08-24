80d0f19e59a707b43426180ec2adece49e51d263
yupix <yupi0982@outlook.jp>
2024-08-24 17:56:15 +0000

    chore: デフォルトのフォントをsawarabiからnoto sans jpに変更

--- app/root.tsx
+++ app/root.tsx
@@ -16,7 +16,7 @@ import { CONFIG } from "./lib/config.server";
 
 export const links: LinksFunction = () => {
   return [
-    { rel: "stylesheet", href: "https://fonts.googleapis.com/css?family=Sawarabi+Gothic" }
+    { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" }
   ]
 }
 
