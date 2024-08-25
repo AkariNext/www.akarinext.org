441ed3a646cd1d5a831c966c5c70dc592677d5eb
yupix <yupi0982@outlook.jp>
2024-08-25 13:14:55 +0000

    feat: リンクカードを表示できるように

--- app/lib/md.server.ts
+++ app/lib/md.server.ts
@@ -1,9 +1,78 @@
 import parseFrontMatter from "front-matter";
-import { createHighlighterCore  } from 'shiki/core'
+import { getSingletonHighlighterCore } from 'shiki/core'
 import getWasm from 'shiki/wasm'
+import { summaly } from '@misskey-dev/summaly';
+
+import { LRUCache } from 'lru-cache'
+import { visitParents } from 'unist-util-visit-parents'
+import Summary from "@misskey-dev/summaly/built/summary";
+
+const cache = new LRUCache<string, any>({
+    max: 500, // キャッシュの最大エントリ数
+});
+
+function youtubePlayer(summary: Summary, url: string) {
+    return `
+    <div style="position: relative; width: 100%; height: 0; padding-top: 56.25%;">
+        <iframe style="border-radius: 4px; position: absolute; top: 0; left: 0; width: 100%; height: 100%;" src="${summary.player.url}" allow=${summary.player.allow.join(';')} height="${summary.player.height}" width="${summary.player.width}" frameborder="0"></iframe>
+    </div>
+    `;
+}
+
+function defaultSummary(summary: Summary, url: string) {
+    return `
+        <div>
+            <a class="link-card" href="${url}" target="_blank">
+                <div class="link-card-content">
+                    <strong>${summary.title}</strong>
+                    ${summary.description ? `<p class="link-card-description">${summary.description}</p>` : ''}
+                    <div class="link-card-footer">
+                        <img src="${summary.icon}"/>
+                        <p>${decodeURI(url)}</p>
+                    </div>
+                </div>
+                <img src="${summary.thumbnail}" class="link-card-thumbnail" alt="thumbnail" />
+            </a>
+        </div>
+                        `
+}
+
+export default function remarkLinkCard() {
+    return async function transformer(tree: any) {
+        const promises: Promise<void>[] = [];
+
+        visitParents(tree, 'link', (node: any, parents: any[]) => {
+            if (parents.filter((parent: any) => parent.type === 'list').length > 0) {
+                return;
+            }
+
+            const url = node.url;
+            const cachedSummary = cache.get(url);
+
+            if (cachedSummary) {
+                node.type = 'html';
+                node.value = cachedSummary.player ? youtubePlayer(cachedSummary, url) : defaultSummary(cachedSummary, url);
+            } else {
+                const promise = summaly(url)
+                    .then(summaryData => {
+                        cache.set(url, summaryData); // キャッシュに保存
+                        node.type = 'html';
+                        node.value = summaryData.player ? youtubePlayer(summaryData, url) : defaultSummary(summaryData, url);
+                    })
+                    .catch(error => {
+                        console.error(`Failed to fetch title for URL: ${url}`, error);
+                    });
+
+                promises.push(promise);
+            }
+        });
+
+        await Promise.all(promises);
+    };
+}
 
 
-let highlighter: Awaited<ReturnType<typeof createHighlighterCore>>;  // 何度も初期化しないためにキャッシュする
+let highlighter: Awaited<ReturnType<typeof getSingletonHighlighterCore>>;  // 何度も初期化しないためにキャッシュする
 let processor: Awaited<ReturnType<typeof getProcessor>>;  // 何度も初期化しないためにキャッシュする
 export async function getProcessor() {
     const [
@@ -25,7 +94,7 @@ export async function getProcessor() {
         import("rehype-autolink-headings"),
         import("@shikijs/rehype/core"),
     ])
-    highlighter = highlighter || await createHighlighterCore({
+    highlighter = highlighter || await getSingletonHighlighterCore({
         themes: [
             import('shiki/themes/github-dark.mjs')
         ],
@@ -47,6 +116,7 @@ export async function getProcessor() {
     return unified()
         .use(remarkParse)
         .use(remarkGfm)
+        .use(remarkLinkCard) // 追加
         .use(remarkRehype, { allowDangerousHtml: true })
         .use(rehypeStringify, { allowDangerousHtml: true })
         .use(rehypeSlug)
