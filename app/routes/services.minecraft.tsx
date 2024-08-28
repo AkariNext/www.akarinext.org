ca1be35c36d9d2424049d9d789b2590766aaf96e
yupix <yupi0982@outlook.jp>
2024-08-28 11:18:59 +0000

    feat: mc鯖のステータスページの表示をdeferで軽量化できるかのお試し
    fix: スマホでmc鯖を見たときgrid-cols-3のままなせいで潰れる

--- app/routes/services.minecraft.tsx
+++ app/routes/services.minecraft.tsx
@@ -1,4 +1,4 @@
-import { json } from '@remix-run/node';
+import { defer } from '@remix-run/node';
 import { IMCServer, MC_SERVERS } from '~/lib/mc_servers';
 import {
 	statusJava,
@@ -6,11 +6,12 @@ import {
 	BedrockStatusResponse,
 	JavaStatusResponse,
 } from 'node-mcstatus';
-import { useLoaderData } from '@remix-run/react';
+import { Await, useLoaderData } from '@remix-run/react';
 import { Badge } from '~/components/ui/badge';
 import { IconQuestionMark } from '@tabler/icons-react';
 import { LRUCache } from 'lru-cache';
 import { Snippet } from '~/components/Snippet';
+import { Suspense } from 'react';
 
 interface IMinecraftProtocolVersion {
 	minecraftVersion: string;
@@ -47,9 +48,9 @@ async function getProtocolVersion(platform: 'pc' | 'bedrock' = 'pc') {
 	return foundCache;
 }
 
-export async function loader() {
+async function getServerStatus() {
 	const foundCache = serverStatus.get('serverStatus');
-	if (foundCache) return json(foundCache);
+	if (foundCache) return foundCache;
 
 	const status = await Promise.all(
 		MC_SERVERS.map(async (server) => {
@@ -86,7 +87,12 @@ export async function loader() {
 	);
 
 	serverStatus.set('serverStatus', status);
-	return json(status);
+	return status;
+}
+
+export async function loader() {
+	const status = getServerStatus();
+	return defer({ status });
 }
 
 function isJavaResponse(
@@ -96,81 +102,91 @@ function isJavaResponse(
 }
 
 export default function Minecraft() {
-	const status = useLoaderData<typeof loader>();
+	const { status } = useLoaderData<typeof loader>();
 	return (
-		<div className="grid grid-cols-3 gap-4">
-			{status.map((server) => {
-				const diff_date = new Date(
-					new Date().getTime() - new Date(server.latestFetchDate).getTime(),
-				);
-				return (
-					<div key={server.info.name} className="border-2 rounded-xl py-4">
-						<div className="flex justify-between border-b items-center px-4 pb-2 mb-2">
-							<div className="flex gap-4 items-center">
-								{isJavaResponse(server.status) && server.status.icon ? (
-									<img
-										src={server.status.icon}
-										className="h-16 w-16 flex-shrink-0"
-										alt={`${server.info.name} icon`}
-									/>
-								) : (
-									<IconQuestionMark className="border rounded-full border-slate-300 h-16 w-16 flex-shrink-0" />
-								)}
-								<h2>{server.info.name}</h2>
-							</div>
-							<div>
-								<Badge
-									className={
-										server.status.online
-											? 'bg-green-600 hover:bg-green-500'
-											: 'bg-red-600 hover:bg-red-500'
-									}
+		<div className="grid grid-col-start-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+			<Suspense fallback={<p>Loading....</p>}>
+				<Await resolve={status}>
+					{(status) =>
+						status.map((server) => {
+							const diff_date = new Date(
+								new Date().getTime() -
+									new Date(server.latestFetchDate).getTime(),
+							);
+							return (
+								<div
+									key={server.info.name}
+									className="border-2 rounded-xl py-4"
 								>
-									{server.status.online ? 'Online' : 'Offline'}
-								</Badge>
-							</div>
-						</div>
-						<div className="pl-4">
-							<h3>アドレス</h3>
-							<Snippet>
-								{server.status.port === 25565
-									? `${server.status.host}`
-									: `${server.status.host}:${server.status.port}`}
-							</Snippet>
-							<h3 className="mt-2">バージョン</h3>
-							<div
-								dangerouslySetInnerHTML={{
-									__html: isJavaResponse(server.status)
-										? server.status.version!.name_raw
-										: server.status.version?.name ?? '?',
-								}}
-							></div>
-							<h3>プレイヤー数</h3>
-							{server.status.players ? (
-								<p>
-									{server.status.players.online} &#47;{' '}
-									{server.status.players.max}
-								</p>
-							) : (
-								<p>不明</p>
-							)}
-							<h3>ステータス取得</h3>
-							<p>
-								{diff_date.getMinutes()}分{diff_date.getSeconds()}秒前
-							</p>
-							<h3>プロトコルバージョン</h3>
-							{server.protocolVersion ? (
-								<p>
-									{server.protocolVersion.majorVersion} (
-									{server.protocolVersion.minecraftVersion})
-								</p>
-							) : (
-								<p>不明</p>
-							)}
-						</div>
-					</div>
-				);
-			})}
+									<div className="flex justify-between border-b items-center px-4 pb-2 mb-2">
+										<div className="flex gap-4 items-center">
+											{isJavaResponse(server.status) && server.status.icon ? (
+												<img
+													src={server.status.icon}
+													className="h-16 w-16 flex-shrink-0"
+													alt={`${server.info.name} icon`}
+												/>
+											) : (
+												<IconQuestionMark className="border rounded-full border-slate-300 h-16 w-16 flex-shrink-0" />
+											)}
+											<h2>{server.info.name}</h2>
+										</div>
+										<div>
+											<Badge
+												className={
+													server.status.online
+														? 'bg-green-600 hover:bg-green-500'
+														: 'bg-red-600 hover:bg-red-500'
+												}
+											>
+												{server.status.online ? 'Online' : 'Offline'}
+											</Badge>
+										</div>
+									</div>
+									<div className="pl-4">
+										<h3>アドレス</h3>
+										<Snippet>
+											{server.status.port === 25565
+												? `${server.status.host}`
+												: `${server.status.host}:${server.status.port}`}
+										</Snippet>
+										<h3 className="mt-2">バージョン</h3>
+										<div
+											dangerouslySetInnerHTML={{
+												__html: isJavaResponse(server.status)
+													? server.status.version!.name_raw
+													: server.status.version?.name ?? '?',
+											}}
+										></div>
+										<h3>プレイヤー数</h3>
+										{server.status.players ? (
+											<p>
+												{server.status.players.online} &#47;{' '}
+												{server.status.players.max}
+											</p>
+										) : (
+											<p>不明</p>
+										)}
+										<h3>ステータス取得</h3>
+										<p>
+											{diff_date.getMinutes()}分{diff_date.getSeconds()}秒前
+										</p>
+										<h3>プロトコルバージョン</h3>
+										{server.protocolVersion ? (
+											<p>
+												{server.protocolVersion.majorVersion} (
+												{server.protocolVersion.minecraftVersion})
+											</p>
+										) : (
+											<p>不明</p>
+										)}
+									</div>
+								</div>
+							);
+						})
+					}
+				</Await>
+			</Suspense>
 		</div>
 	);
 }
