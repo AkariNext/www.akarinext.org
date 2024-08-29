ac1ce01b73af85bb946ce82bb09e8464ddbdad8a
yupix <yupi0982@outlook.jp>
2024-08-29 16:22:57 +0000

    feat: prismaに乗り換える close #243

--- drizzle/0001_breezy_norman_osborn.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "file" ADD PRIMARY KEY ("id");--> statement-breakpoint
-ALTER TABLE "file" ALTER COLUMN "id" SET DATA TYPE varchar;--> statement-breakpoint
-ALTER TABLE "file" ALTER COLUMN "id" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "file" ALTER COLUMN "author_id" SET DATA TYPE varchar;--> statement-breakpoint
-ALTER TABLE "users" ALTER COLUMN "id" SET DATA TYPE varchar;
--- src/migrate.ts
+++
@@ -1,8 +0,0 @@
-import 'dotenv/config';
-
-import { migrate } from 'drizzle-orm/postgres-js/migrator';
-import { connection, db } from '~/lib/db.server';
-
-await migrate(db, { migrationsFolder: './drizzle' });
-
-await connection.end();
