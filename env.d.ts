be7170f7216661d4504535b0a0665cde4c6553b8
yupix <yupi0982@outlook.jp>
2024-08-29 15:55:45 +0000

    fix: 何のためにあるのかよくわからないenvの定義を削除

--- env.d.ts
+++ env.d.ts
@@ -1,8 +1,3 @@
 /// <reference types="@remix-run/node" />
 /// <reference types="vite/client" />
 
-declare module "*.mdx" {
-    let MDXComponent: (props: any) => JSX.Element;  // eslint-disable-line @typescript-eslint/no-explicit-any
-    export const frontmatter: any;  // eslint-disable-line @typescript-eslint/no-explicit-any
-    export default MDXComponent;
-  }
