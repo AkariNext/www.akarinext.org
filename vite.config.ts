909c1a2a74886782d21d98d54bc175029b14e79a
yupix <yupi0982@outlook.jp>
2024-11-05 22:55:56 +0900

    fix: singlefetchのフラグ名が変わってたので修正

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,20 @@ import mdx from '@mdx-js/rollup';
 import { vitePlugin as remix } from '@remix-run/dev';
 import remarkFrontmatter from 'remark-frontmatter';
 import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
-import { installGlobals } from '@remix-run/node';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import arraybuffer from 'vite-plugin-arraybuffer';
 import { devErrorBoundary } from '@metronome-sh/dev-error-boundary';
 import { flatRoutes } from 'remix-flat-routes';
+import { installGlobals } from '@remix-run/node';
 
 installGlobals({ nativeFetch: true });
+
+declare module "@remix-run/node" {
+    interface Future {
+        v3_singleFetch: true;
+    }
+}
 
 export default defineConfig({
 	build: {
@@ -23,7 +29,7 @@ export default defineConfig({
 		remix({
 			ignoredRouteFiles: ['**/*'],
 			serverModuleFormat: 'esm',
-			future: { unstable_singleFetch: true },
+			future: { v3_singleFetch: true },
 			routes: async (defineRoutes) => {
 				return flatRoutes('routes', defineRoutes, {
 					ignoredRouteFiles: ['.*', '**/*.css', '**/__*.*'],
